4af9ca4bb6322a286c4ce7c29a293bec7ebfdd59
randaaz <randaaziz972@gmail.com>
2024-02-16 02:44:54 -0800

    test fro the file city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,10 +2,11 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import os
 
 
 class test_City(test_basemodel):
-    """ """
+    """  Test class for the City class in the HBNB project"""
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -14,11 +15,15 @@ class test_City(test_basemodel):
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """  check the 'state_id' attribute of the City class."""
         new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        self.assertEqual(type(new.state_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_name(self):
-        """ """
+        """ Test method to check the 'name' attribute of the City class."""
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
