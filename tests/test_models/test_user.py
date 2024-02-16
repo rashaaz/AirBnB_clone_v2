9ac239f3891db24586fb375603736fb072d333c2
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-16 01:58:48 -0800

    add test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,33 +2,42 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+import os
 
 
 class test_User(test_basemodel):
-    """ """
+    """Test cases for the User class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Constructor for test_User class"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """Test the first_name attribute"""
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(type(new.first_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_last_name(self):
-        """ """
+        """Test the last_name attribute"""
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(type(new.last_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_email(self):
-        """ """
+        """Test the email attribute"""
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertEqual(type(new.email), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_password(self):
-        """ """
+        """Test the password attribute"""
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(type(new.password), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
