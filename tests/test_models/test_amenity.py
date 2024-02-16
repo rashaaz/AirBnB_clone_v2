62846ddefa931c90bce68117897f855a7cc09da6
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-16 03:04:09 -0800

    add test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,18 +2,21 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import os
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Test cases for the Amenity class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Constructor for test_Amenity class """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Test the name attribute """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
