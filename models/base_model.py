d7c2972c0e32602695b75ac58f30b1950b696a6c
randaaz <randaaziz972@gmail.com>
2024-02-17 22:09:44 -0800

    modfy

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,12 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models
+    """Base class for HBNB project ORM models.
 
-    Attributes:
-        id (sqlalchemy String): The BaseModel id.
-        created_at (sqlalchemy DateTime): The datetime at creation.
-        updated_at (sqlalchemy DateTime): The datetime of last update.
+        Attributes:
+            id (str): Unique identifier for the instance.
+            created_at (datetime): Timestamp of instance creation.
+            updated_at (datetime): Timestamp of instance last update.
     """
     id = Column(String(60),
                 nullable=False,
@@ -29,7 +29,7 @@ class BaseModel:
                         default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Initializes a new instance of the BaseModel class."""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -49,19 +49,19 @@ class BaseModel:
                     setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
-        """Returns a string representation of the instance"""
+        """Returns a string representation of the BaseModel instance"""
         return '[{}] ({}) {}'.format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
+        """Returns a dictionary representation of the BaseModel instance."""
         dct = self.__dict__.copy()
         dct['__class__'] = self.__class__.__name__
         for k in dct:
@@ -72,6 +72,6 @@ class BaseModel:
         return dct
 
     def delete(self):
-        '''deletes the current instance from the storage'''
+        '''Deletes the current instance from the storage system.'''
         from models import storage
         storage.delete(self)
