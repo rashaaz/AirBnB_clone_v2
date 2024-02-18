9293c6ac56d733493c2ec71ca0c605f8af76dabb
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-17 22:48:41 -0800

    modfy

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+""" Review module for the HBNB project. """
 from sqlalchemy.sql.schema import ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
