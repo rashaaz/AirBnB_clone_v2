dabdb7ebaf284ffc6bcdc6787359bdc063aefbaa
rashaaz <rashaabdelaziz111@gmail.com>
2024-02-17 22:49:12 -0800

    modfy

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from models import storage_type
 
 
 class Review(BaseModel, Base):
-    """ Review classto store review information """
+    """ Review classto store review information. """
     __tablename__ = 'reviews'
     if storage_type == 'db':
         text = Column(String(1024), nullable=False)
